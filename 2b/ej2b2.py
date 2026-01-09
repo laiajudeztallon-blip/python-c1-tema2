9077f533af8aaa03b01f41ce9f529594ab272b71
laiajudeztallon-blip <laiajudeztallon@gmail.com>
2026-01-09 11:28:34 +0100

    Update ej2b2.py

--- 2b/ej2b2.py
+++ 2b/ej2b2.py
@@ -21,7 +21,17 @@ def create_app():
     app = Flask(__name__)
 
     # Aquí debes implementar los endpoints solicitados
-
+    @app.route('/hello')
+    def hello():
+        return "¡Hola mundo!", 200, {'Content-Type': 'text/plain; charset=utf-8'}
+    
+    @app.route('/goodbye')
+    def goodbye():
+        return "¡Adiós mundo!", 200, {'Content-Type': 'text/plain; charset=utf-8'}
+    
+    @app.route('/greet/<nombre>')
+    def greet(nombre):
+        return f"¡Hola, {nombre}!", 200, {'Content-Type': 'text/plain; charset=utf-8'}
     return app
 
 if __name__ == '__main__':
