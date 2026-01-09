7a3c9abba40c58a075424fcdab3dc20834a0e1c5
laiajudeztallon-blip <laiajudeztallon@gmail.com>
2026-01-09 11:31:34 +0100

    Update ej2c1.py

--- 2c/ej2c1.py
+++ 2c/ej2c1.py
@@ -51,7 +51,11 @@ def create_app():
         - Si no existe: devuelve un error con código 404 (Not Found)
         """
         # Implementa este endpoint
-        pass
+        or product in products:
+            if product["id"] == product_id:
+                return jsonify(product), 200
+        return jsonify({"error": "Product not found"}), 404
+
 
 
     return app
